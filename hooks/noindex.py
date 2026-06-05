6021bec77a0f2fa8a66f9e1aedc243d46595d39c
duyanhdinh03 <duyanhk94lc@gmail.com>
2026-06-05 10:57:39 +0700

    Add noindex and robots controls for docs

---
+++ hooks/noindex.py
@@ -0,0 +1,14 @@
+﻿NOINDEX_META = """
+  <meta name="robots" content="noindex,nofollow,noarchive,nosnippet">
+  <meta name="googlebot" content="noindex,nofollow,noarchive,nosnippet">
+  <meta name="bingbot" content="noindex,nofollow,noarchive,nosnippet">
+"""
+
+def on_post_page(output, page, config):
+    if '<meta name="robots"' in output:
+        return output
+
+    if "<head>" in output:
+        return output.replace("<head>", "<head>" + NOINDEX_META, 1)
+
+    return output
